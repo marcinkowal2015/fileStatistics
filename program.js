ca21a6cefcc46b603a62d0c1dd2c9fbf43279e47
Marcin Kowal <marcink@medius-group.com>
2017-09-11 18:07:15 +0200

    script for starting mongo added

--- program.js
+++ program.js
@@ -3,8 +3,10 @@ const connection = require('./connectionProvider');
 
 
 fileStatistics.countStats().then(stats => {
+    console.log(stats);
     connection.insertOneInCollection('fileStats', stats);
 });
 fileStatistics.statsDetails().then(details => {
+    console.log(details);
     connection.insertOneInCollection('fileStatsDetails', details);
 });
